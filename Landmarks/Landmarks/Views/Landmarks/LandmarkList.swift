4a61b2504d2cbd919ec9c18776b7864b61628907
Thibault Friedrich <thibault.friedrich@gmail.com>
2022-04-29 11:31:29 -0400

    feat: add favorites

---
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Thibault Friedrich on 2022-04-29.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    @EnvironmentObject var modelData: ModelData
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter  { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (3nd generation)", "iPhone 12 Pro Max", "iPad Pro (12.9-inch) (5th generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .environmentObject(ModelData())
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
